2e018924e8a09f4c2b503e2b6cdbfbf5357cb687
paragraph <o.matzner@gmail.com>
2023-06-21 10:24:58 +0200

    new upload

--- js/firebase.js
+++ js/firebase.js
@@ -16,24 +16,24 @@ import {
 // config
 const firebaseConfig = {
 
-  // NEW DB API KEY
-  apiKey: "AIzaSyCUGdgKzVb6_cJyav4V5U8EyO6iJNmZm0s",
-  authDomain: "co-lab-16feb.firebaseapp.com",
-  projectId: "co-lab-16feb",
-  storageBucket: "co-lab-16feb.appspot.com",
-  messagingSenderId: "827072280835",
-  appId: "1:827072280835:web:210d9802f03c7286181136",
-  measurementId: "G-SQRV99ZX8S"
+  // // NEW DB API KEY
+  // apiKey: "AIzaSyCUGdgKzVb6_cJyav4V5U8EyO6iJNmZm0s",
+  // authDomain: "co-lab-16feb.firebaseapp.com",
+  // projectId: "co-lab-16feb",
+  // storageBucket: "co-lab-16feb.appspot.com",
+  // messagingSenderId: "827072280835",
+  // appId: "1:827072280835:web:210d9802f03c7286181136",
+  // measurementId: "G-SQRV99ZX8S"
   
 
-// //OLD DB API KEY
-//     apiKey: "AIzaSyD5fO7vcmtPVbsruyOrF-YDmhNDKGg0LlI",
-//     authDomain: "co-lab-prj.firebaseapp.com",
-//     projectId: "co-lab-prj",
-//     storageBucket: "co-lab-prj.appspot.com",
-//     messagingSenderId: "983517005444",
-//     appId: "1:983517005444:web:783087360a20eeb4bba4d1",
-//     measurementId: "G-TBHR1NL4MF"
+//OLD DB API KEY
+    apiKey: "AIzaSyD5fO7vcmtPVbsruyOrF-YDmhNDKGg0LlI",
+    authDomain: "co-lab-prj.firebaseapp.com",
+    projectId: "co-lab-prj",
+    storageBucket: "co-lab-prj.appspot.com",
+    messagingSenderId: "983517005444",
+    appId: "1:983517005444:web:783087360a20eeb4bba4d1",
+    measurementId: "G-TBHR1NL4MF"
   
 };
 
